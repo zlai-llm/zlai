4ce806e18ccab108339a9a53e5a590711f62da31
chensy <chensy.cao@foxmail.com>
2024-06-25 13:55:01 +0800

    20240607: add silicon_flow

---
+++ test/llms/silicon_flow.py
@@ -0,0 +1,24 @@
+import unittest
+from zlai.llms.silicon_flow import *
+from zlai.llms.generate_config.silicon_flow import *
+
+
+class TestMokeAliModels(unittest.TestCase):
+    """"""
+    def test_loop_config(self):
+        """"""
+        config = [
+            Qwen2Instruct7BGenerateConfig,
+            Qwen2Instruct15BGenerateConfig,
+            Qwen15Chat7BGenerateConfig,
+            GLM3Chat6BGenerateConfig,
+            GLM4Chat9BGenerateConfig,
+            Yi15Chat6BGenerateConfig,
+            Yi15Chat9BGenerateConfig,
+        ]
+        for gen_config in config:
+            print(gen_config.__name__)
+            llm = SiliconFlow(generate_config=gen_config())
+            data = llm.generate(query="1+1=")
+            print(data.choices[0].message.content)
+
