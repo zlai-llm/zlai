7959687971cc8d68c5876507406a91db1b76358a
chensy <chensy.cao@foxmail.com>
2024-06-28 12:54:42 +0800

    20240628: update

--- zlai/tools/fund/fund.py
+++ zlai/tools/fund/fund.py
@@ -3,10 +3,11 @@ import json
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
-from typing import Dict, Union, Annotated
+from typing import List, Dict, Union, Optional, Annotated
 from pydantic import BaseModel, Field
 
 from ...parse import ParseList, ParseDict
+from .utils import *
 
 
 __all__ = [
@@ -14,7 +15,9 @@ __all__ = [
     "CurrentFundData",
     "get_fund_basic_info",
     "get_current_fund",
-    "get_all_fund",
+    "search_fund",
+    "get_fund_company",
+    "get_fund_history",
 ]
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
@@ -27,6 +30,34 @@ def get_url(url):
     res = requests.get(url, headers=headers)
     res.encoding = "utf-8"
     return res.text
+
+
+def search_fund(
+        fund_name: Annotated[Optional[str], '基金名称', True] = None,
+        fund_code: Annotated[Optional[str], '基金代码', False] = None,
+        max_: Annotated[Optional[int], '最大返回数量', False] = 10,
+) -> Dict:
+    """
+    依据用户传入的基金名称或者基金代码，获取基金代码、基金拼音简写、基金类型、基金拼音全称等信息
+
+    :param fund_name: 基金名称
+    :param fund_code: 基金代码
+    :param max_: 最大返回数量
+    :return:
+    """
+    url = "http://fund.eastmoney.com/js/fundcode_search.js"
+    info = get_url(url)
+    info = ParseList.greedy_list(info)[0]
+    columns = ['基金代码', '基金拼音简写', '基金名称', '基金类型', '基金拼音全称']
+    df = pd.DataFrame(info, columns=columns, dtype=str)
+    if fund_code:
+        data = df[df["基金代码"] == fund_code].to_dict("records")
+        return data
+    elif fund_name:
+        data = df[df["基金名称"].str.contains(fund_name)].to_dict("records")[:max_]
+        return data
+    else:
+        raise ValueError("fund_code or fund_name must be provided")
 
 
 class CurrentFundData(BaseModel):
@@ -68,7 +99,13 @@ def get_fund_basic_info(
         fund_code: Annotated[str, '基金代码', True] = '000001',
 ) -> Dict:
     """
-    依据用户传入的基金代码，获取基金的基本信息。
+    依据基金代码获取基金的基本信息。基本信息包括:
+        ['基金全称', '基金简称', '基金代码', '基金类型', '发行日期', '成立日期/规模', '资产规模', '份额规模', '基金管理人',
+        '基金托管人', '基金经理人', '成立来分红', '管理费率', '托管费率',  '销售服务费率', '最高认购费率', '最高申购费率',
+        '最高赎回费率', '业绩比较基准', '跟踪标的']
+
+    :param fund_code: 基金代码
+    :return: 基金基本信息
     """
     url = f"https://fundf10.eastmoney.com/jbgk_{fund_code}.html"
     text = get_url(url)
@@ -97,16 +134,60 @@ def get_current_fund(
         return f"未查询到 {fund_code} 的数据。"
 
 
-def get_all_fund(
-        fund_name: Annotated[str, '基金名称', True] = '000001',
-) -> Dict:
+def get_fund_company(
+        company_name: Annotated[Optional[str], '基金公司名称', True] = None,
+        company_code: Annotated[Optional[str], '基金公司代码', True] = None,
+) -> List[Dict]:
     """
-    依据用户传入的基金名称，获取基金代码、基金拼音简写、基金类型、基金拼音全称等信息
+    给定基金公司代码查询基金公司名称，或给定基金公司名称查询基金公司代码
+
+    :param company_name: 基金公司名称
+    :param company_code: 基金公司代码
+    :return:
     """
-    url = "http://fund.eastmoney.com/js/fundcode_search.js"
+    url = "http://fund.eastmoney.com/js/jjjz_gs.js"
     info = get_url(url)
-    info = ParseList.eval_list(info)
-    columns = ['基金代码', '基金拼音简写', '基金名称', '基金类型', '基金拼音全称']
-    df = pd.DataFrame(info, columns=columns)
-    data = df[df["基金名称"].str.contains(fund_name)].head(5).to_dict("records")
+    data = ParseList.greedy_list(info)[0]
+    columns = ['基金公司代码', '基金公司名称']
+    df_data = pd.DataFrame(data, columns=columns)
+    if company_code:
+        data = df_data[df_data["基金公司代码"] == company_code].to_dict("records")
+        return data
+    elif company_name:
+        data = df_data[df_data["基金公司名称"].str.contains(company_name)].to_dict("records")
+        return data
+    else:
+        raise ValueError("fund_code or fund_name must be provided")
+
+
+def get_fund_history(
+        fund_code: Annotated[str, '基金代码', True],
+        start_date: Annotated[str, '查询开始日期', True],
+        end_date: Annotated[str, '查询结束日期', True],
+) -> str:
+    """
+    
+    :param fund_code: 基金代码
+    :param end_date: 查询开始日期, yyyy-MM-dd
+    :param start_date: 查询结束日期, yyyy-MM-dd
+    :return: 
+    """
+    columns = ['FSRQ', 'DWJZ', 'LJJZ', 'SDATE', 'ACTUALSYI', 'NAVTYPE', 'JZZZL', 'SGZT',
+               'SHZT', 'FHFCZ', 'FHFCBZ', 'DTYPE', 'FHSP']
+    col_mapping = {
+        "FSRQ": "净值日期", "DWJZ": "单位净值", "LJJZ": "累计净值",
+        "JZZZL": "日增长率", "SGZT": "申购状态", "SHZT": "赎回状态",
+    }
+    jq_callback = jquery_mock_callback()
+    url = f"https://api.fund.eastmoney.com/f10/lsjz?callback={jq_callback}" \
+          f"&fundCode={fund_code}&pageIndex={1}&pageSize={100}" \
+          f"&startDate={start_date}&endDate={end_date}&_={get_current_timestamp()}"
+
+    text = get_url(url)
+    null, false = None, False
+    fund_info = eval(text.replace(jq_callback, ''))
+    df_fund = pd.DataFrame(fund_info.get('Data').get('LSJZList'))
+    df_fund = df_fund[columns]
+    df_fund = df_fund[list(col_mapping.keys())].rename(columns=col_mapping)
+    data = df_fund.to_markdown(index=False)
     return data
---
+++ zlai/tools/fund/utils.py
@@ -0,0 +1,20 @@
+import time
+import random
+
+__all__ = [
+    "Constant",
+    "get_current_timestamp",
+    "jquery_mock_callback",
+]
+
+
+class Constant:
+    jQuery_Version = "1.8.3"
+
+
+def get_current_timestamp() -> int:
+    return int(round(time.time() * 1000))
+
+
+def jquery_mock_callback() -> str:
+    return f'jQuery{(Constant.jQuery_Version + str(random.random())).replace(".", "")}_{str(get_current_timestamp() - 1000)}'
