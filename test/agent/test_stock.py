80a8f5433e1d5ccffb70fd4bae6fb229cbc02469
chensy <chensy.cao@foxmail.com>
2024-07-01 10:19:29 +0800

    20240701: update test

---
+++ test/agent/test_stock.py
@@ -0,0 +1,31 @@
+import unittest
+from zlai.tools import *
+
+
+class TestStock(unittest.TestCase):
+    """"""
+    def setUp(self):
+        """"""
+
+    def test_kline_data(self):
+        """"""
+        data = get_stock_kline_data()
+        print(data)
+
+    def test_get_future_data(self):
+        """"""
+        # data = get_futures_data(symbol="AG2408", _type="1min")
+        # print(data.shape)
+        # print(data.head())
+        #
+        # data = get_futures_data(symbol="AG2408", _type="5min")
+        # print(data.shape)
+        # print(data.head())
+        #
+        # data = get_futures_data(symbol="AG2408", _type="1day")
+        # print(data.shape)
+        # print(data.head())
+
+        data = get_futures_data(symbol="AG2408", _type="5day")
+        print(data.shape)
+        print(data.head())
---
+++ test/llms/test_hunyuan.py
@@ -0,0 +1,28 @@
+import os
+import unittest
+from zlai.llms import HunYuan
+from zlai.llms.generate_config.hunyuan import *
+
+
+class TestHunYuan(unittest.TestCase):
+    """"""
+    def test_hunyuan(self):
+        """"""
+        config = [
+            HunYuanLiteGenerateConfig,
+            HunYuanStandardGenerateConfig,
+            HunYuanStandard256KGenerateConfig,
+            HunYuanProGenerateConfig,
+        ]
+        for gen_config in config:
+            llm = HunYuan(generate_config=gen_config())
+            completion = llm.generate(query="1+1=")
+            print(f"{gen_config.__name__.replace('GenerateConfig', '')}: {completion.choices[0].message.content}")
+            print()
+
+    def test_stream(self):
+        """"""
+        llm = HunYuan(generate_config=HunYuanLiteGenerateConfig(stream=True))
+        completions = llm.generate(query="1+1=")
+        for completion in completions:
+            print(completion.choices[0].message.content)
