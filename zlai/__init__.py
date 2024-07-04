891a06f23f6572abd515dd6d3db3fa691c392acd
chensy <chensy.cao@foxmail.com>
2024-07-04 16:25:43 +0800

    20240703: update new version.

--- zlai/__init__.py
+++ zlai/__init__.py
@@ -1,10 +1,10 @@
-__all__ = ["__version__"]
-__version__ = "0.3.95"
+# __all__ = ["__version__"]
+# __version__ = "0.3.95"
 
-# from importlib import metadata
-#
-# try:
-#     __version__ = metadata.version(__package__)
-# except metadata.PackageNotFoundError:
-#     # Case where package metadata is not available.
-#     __version__ = ""
+from importlib import metadata
+
+try:
+    __version__ = metadata.version(__package__)
+except metadata.PackageNotFoundError:
+    # Case where package metadata is not available.
+    __version__ = ""
