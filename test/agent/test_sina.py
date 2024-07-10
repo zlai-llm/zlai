5907a81b1fc464a398eec3d8b1c49ac93e21b4bf
chensy <chensy.cao@foxmail.com>
2024-07-10 15:54:12 +0800

    20240705: update sina test

--- test/agent/test_sina.py
+++ test/agent/test_sina.py
@@ -1,6 +1,7 @@
 import unittest
-from typing import Literal
+from zlai.llms import Zhipu, GLM4AirGenerateConfig
 from zlai.tools.stock.sina import *
+from zlai.agent import *
 
 
 class TestSina(unittest.TestCase):
@@ -9,7 +10,7 @@ class TestSina(unittest.TestCase):
         """"""
         markets = ["sh_a", "sh_b", "sz_a", "sz_b", "hk", "warn"]
         for market in markets:
-            data = get_current_stock_rank(market=market, return_type="DataFrame")
+            data = get_current_stock_rank(market=market)
             print(f"{market}: {data.shape}")
 
     def test_get_stock_rank_sort(self):
@@ -19,8 +20,18 @@ class TestSina(unittest.TestCase):
             "open", "high", "low", "volume", "amount",
         ]
         for sort in sorts:
-            data = get_current_stock_rank(sort_by=sort, return_type="Markdown")
+            data = get_current_stock_rank(sort_by=sort)
             print(f"{sort}")
             print(data)
             print()
 
+    def test_agent(self):
+        """"""
+        llm = Zhipu(generate_config=GLM4AirGenerateConfig())
+        tool_list = [get_current_stock_rank]
+        tools = Tools(tool_list=tool_list)
+
+        agent = ToolsAgent(llm=llm, tools=tools, verbose=True)
+        task_completion = agent("帮我分析一下上证股市的涨幅最大的10只股票。")
+
+        print(task_completion.content)
