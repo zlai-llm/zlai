4639e1bd25ed398315e09e19f96085226d7b458b
chensy <chensy.cao@foxmail.com>
2024-07-23 10:34:19 +0800

    20240721: remove langchain prompt

--- test/prompt/prompt.py
+++ test/prompt/prompt.py
@@ -1,6 +1,6 @@
 import unittest
 from zlai.embedding import Embedding, PretrainedEmbedding
-from zlai.prompt import MessagesPrompt
+from zlai.prompt import MessagesPrompt, PromptTemplate
 from zlai.schema import SystemMessage, UserMessage, AssistantMessage
 
 
@@ -34,3 +34,25 @@ class TestPromptFewShotRerank(unittest.TestCase):
         messages = messages_prompt.prompt_format(content="1+3=")
         for message in messages:
             print(f"{message.role}: {message.content}")
+
+
+class TestPromptTemplate(unittest.TestCase):
+    """"""
+    def test_template(self):
+        """"""
+        PROMPT_SUMMARY_TMP = """Content: {content}\nQuestion: {question}"""
+
+        summary_prompt = PromptTemplate(
+            input_variables=["content", "question"],
+            template=PROMPT_SUMMARY_TMP)
+
+        system_message = SystemMessage(
+            content="""You were a helpful assistant, answering questions using the reference content provided.""")
+
+        print(summary_prompt.format(content="1+3=", question="What is the answer?"))
+        print()
+        print(summary_prompt.format(content="1+3=", question="What is the answer?").to_string())
+        print()
+        print(summary_prompt.format(content="1+3=", question="What is the answer?").to_messages(role="user"))
+        print()
+        print(summary_prompt.format_prompt(content="1+3=", question="What is the answer?").to_messages(role="user"))
