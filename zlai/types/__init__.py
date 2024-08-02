753235701fa94697e46ff9230f9cb379dec90283
chensy <chensy.cao@foxmail.com>
2024-08-02 11:01:18 +0800

    20240724: update types

--- zlai/types/__init__.py
+++ zlai/types/__init__.py
@@ -1,4 +1,6 @@
 from .messages import *
 from .function_call import *
-from .chat_completion import *
 from .completion_usage import *
+from .chat_completion import *
+from .chat_completion_chunk import *
+from .chat_completion_token_logprob import *
