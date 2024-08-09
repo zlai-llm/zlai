a899a4da3f08ca5cb42268e622fc02ae57538810
chensy <chensy.cao@foxmail.com>
2024-08-09 10:36:50 +0800

    20240724: update.

---
+++ zlai/utils/image.py
@@ -0,0 +1,15 @@
+import io
+import base64
+from PIL import Image
+
+
+__all__ = [
+    "trans_bs64_to_image"
+]
+
+
+def trans_bs64_to_image(bs64: str):
+    """"""
+    img_bytes = base64.b64decode(bs64)
+    image = Image.open(io.BytesIO(img_bytes))
+    return image
