51a470ea209204ec329f00d541e7bc3d04e2d2eb
chensy <chensy.cao@foxmail.com>
2024-08-09 09:15:51 +0800

    20240724: update.

--- zlai/models/types/images_generations/kolors.py
+++ zlai/models/types/images_generations/kolors.py
@@ -13,6 +13,11 @@ class KolorsImageGenerateConfig(ImageGenerateConfig):
     height: Optional[int] = Field(default=1024, description="The height of the image to generate.")
     width: Optional[int] = Field(default=1024, description="The width of the image to generate.")
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if "size" in kwargs:
+            self.width, self.height = [int(item) for item in kwargs["size"].split("x")]
+
     def gen_kwargs(self) -> dict:
         """"""
         return {
