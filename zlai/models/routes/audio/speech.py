34e8ed0ab67c6de7b7b8f7084d6a6e4314b3c8b0
chensy <chensy.cao@foxmail.com>
2024-08-15 15:30:02 +0800

    20240724: add audio

--- zlai/models/routes/audio/speech.py
+++ zlai/models/routes/audio/speech.py
@@ -3,23 +3,43 @@ from fastapi import HTTPException, Response
 from zlai.utils import pkg_config
 from zlai.models.utils import load_model_config, get_model_config
 
+from zlai.models.tts.generation import LoadModelAudio
 from zlai.models.types.audio import *
 from ....models import app, logger
 
 
 __all__ = [
-    "embeddings"
+    "audio_speech"
 ]
 
 
 @app.post("/audio/speech")
-def embeddings(
+def audio_speech(
     request: SpeechRequest
 ):
     """"""
-    print(request)
-    with open("/Users/chensy/OneDrive/代码及文档/zlai/zlai/test/test_data/audio/audio.wav", "rb") as audio:
-        content = audio.read()
-    response = Response(content=content, media_type="audio/wav",)
-    print(response)
-    return response
+    models_config_path = os.path.join(pkg_config.cache_path, "models_config.yml")
+    if not os.path.exists(models_config_path):
+        resp_content = f"[AudioSpeech] Models config path: {models_config_path} not exists."
+        logger.error(resp_content)
+        raise HTTPException(status_code=400, detail=resp_content)
+    else:
+        logger.info(f"[AudioSpeech] Models config path: {models_config_path}")
+        models_config = load_model_config(path=models_config_path)
+        models_config = models_config.get("models_config")
+        model_config = get_model_config(model_name=request.model, models_config=models_config)
+        if model_config is None:
+            raise HTTPException(status_code=400, detail="Invalid request, model not exists.")
+        else:
+            logger.info(f"[AudioSpeech] Model config: {model_config}")
+        try:
+            model = LoadModelAudio(
+                models_config=models_config, model_name=request.model, logger=logger)
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=f"Load Model Error: {e}")
+        try:
+            wav_binary = model.generate()
+            response = Response(content=wav_binary, media_type="audio/wav", )
+            return response
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=f"Generate Error: {e}")
