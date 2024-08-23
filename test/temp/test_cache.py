bc38fe28a59bdb8d225a6401d68f01bf198d4974
chensy <chensy.cao@foxmail.com>
2024-08-23 14:33:16 +0800

    20240822: update load

--- test/temp/test_cache.py
+++ test/temp/test_cache.py
@@ -1,10 +1,5 @@
 import unittest
-from cachetools import cached, TTLCache
-
-
-@cached(cache=TTLCache(ttl=300, maxsize=2))
-def add(a, b):
-    return a + b
+from cachetools import cached, LRUCache
 
 
 class TestCache(unittest.TestCase):
@@ -12,13 +7,13 @@ class TestCache(unittest.TestCase):
 
     def test_(self):
         """"""
-        @cached(cache=TTLCache(maxsize=4, ttl=100))
+        @cached(cache=LRUCache(maxsize=4))
         def load_glm4(model_path, max_memory=None):
             return None, None
 
         a = load_glm4("/a", tuple(("0", "30GB")))
         print(list(load_glm4.cache))
-        b = load_glm4("/b",)
+        b = load_glm4("/b", )
         print(list(load_glm4.cache))
         for item in list(load_glm4.cache):
             print(item[0])
