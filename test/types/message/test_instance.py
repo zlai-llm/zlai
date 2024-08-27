a4f378d4887b0085993853dcd95034cef6465084
chensy <chensy.cao@foxmail.com>
2024-08-27 16:53:35 +0800

    20240822:

---
+++ test/types/message/test_instance.py
@@ -0,0 +1,8 @@
+import unittest
+from pydantic import BaseModel, Field
+from typing import List, Literal
+
+
+class TestImageGenerateConfig(unittest.TestCase):
+    def test_image_generate_config(self):
+        """"""
