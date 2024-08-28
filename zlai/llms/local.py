2307a4df41238c2c18675cd0e4e338569ec6b68f
chensy <chensy.cao@foxmail.com>
2024-08-28 11:10:44 +0800

    20240822: 迁移老版本模型配置

--- zlai/llms/local.py
+++ zlai/llms/local.py
@@ -1,8 +1,11 @@
 import os
-from typing import Any, List, Literal, Optional
+from typing import Any, List, Union, Iterable, Literal, Optional
 from zlai.types.messages import TypeMessage
 from zlai.types.generate_config.completion import GenerateConfig
+from zlai.types.chat import ChatCompletion
+from zlai.types.request.completion import ChatCompletionRequest
 from .generate import OpenAICompletion
+from ..schema import Message
 
 
 __all__ = ["LocalCompletion"]
@@ -16,6 +19,8 @@ class LocalCompletion(OpenAICompletion):
             generate_config: Optional[GenerateConfig] = None,
             api_key: Optional[str] = "EMPTY",
             messages: Optional[List[TypeMessage]] = None,
+            model: Optional[str] = None,
+            stream: Optional[bool] = False,
             output: Literal["completion", "message", "str"] = "completion",
             verbose: Optional[bool] = False,
             api_key_name: Optional[str] = "BASE_URL",
@@ -29,7 +34,23 @@ class LocalCompletion(OpenAICompletion):
             api_key_name=api_key_name,
             *args, **kwargs)
         self.messages = messages
+        self.model = model
+        self.stream = stream
         self.verbose = verbose
         self.output = output
         self.parse_info = []
         self._create_client()
+
+    def generate(
+            self,
+            query: Optional[str] = None,
+            messages: Optional[List[TypeMessage]] = None,
+    ) -> Union[ChatCompletion, Message, Iterable[ChatCompletion], str]:
+        """"""
+        messages = self._make_messages(query=query, messages=messages)
+        messages = [message.to_dict() for message in messages]
+        request = ChatCompletionRequest.model_validate({
+            **self.generate_config.model_dump(),
+            **{"messages": messages, "model": self.model, "stream": self.stream}})
+        completion = self.client.chat.completions.create(**request.gen_kwargs())
+        return completion
