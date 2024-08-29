d7ed079d2cfb799f2a60b27724fb14d42c12efb7
chensy <chensy.cao@foxmail.com>
2024-08-29 13:21:00 +0800

    20240828: update message

--- zlai/types/messages/function.py
+++ zlai/types/messages/function.py
@@ -8,7 +8,6 @@ from .base import Message
 __all__ = [
     "ObservationMessage",
     "FunctionMessage",
-    "ToolMessage",
     "ToolsMessage",
 ]
 
@@ -16,7 +15,7 @@ __all__ = [
 class ObservationMessage(Message):
     """"""
     model_config = ConfigDict(arbitrary_types_allowed=True)
-    role: Literal["observation"] = Field("observation", description="角色")
+    role: Literal["observation"] = Field(default="observation", description="角色")
     content: Union[str, List, Dict, DataFrame] = Field(..., description="对话内容")
 
     def show_streamlit(self) -> None:
@@ -24,13 +23,14 @@ class ObservationMessage(Message):
         show_observation(self.content)
 
 
-class FunctionMessage(ObservationMessage):
-    pass
-
-
-class ToolMessage(ObservationMessage):
+class FunctionMessage(Message):
     """"""
-    function_call: bool = Field(default=True, description="是否为工具调用")
+    role: Literal["function"] = Field(default="function", description="角色")
+    content: Union[str] = Field(..., description="对话内容")
+
+    def show_streamlit(self) -> None:
+        _ = self._validate_streamlit()
+        show_observation(self.content)
 
 
 class ToolsMessage(Message):
@@ -38,3 +38,7 @@ class ToolsMessage(Message):
     role: Literal["tool"] = Field(default="tool", description="角色")
     content: str = Field(default="", description="对话内容")
     tool_call_id: Optional[Union[int, str, dict]] = Field(default=None, description="id")
+
+    def show_streamlit(self) -> None:
+        _ = self._validate_streamlit()
+        show_observation(self.content)
